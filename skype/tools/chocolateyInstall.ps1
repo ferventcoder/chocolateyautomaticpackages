55a5bc6e922a5a22bdd8a6f68e151f1631982898
TomOne <user.email@fake.com>
2013-06-05 20:03:47 +0200

    use better skype download link which always points to the latest version

--- skype/tools/chocolateyInstall.ps1
+++ skype/tools/chocolateyInstall.ps1
@@ -1,3 +1,2 @@
-#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/3694814915aaa38100bfa0933f948e65/partner/59/SkypeSetup.exe'
-#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' '{{DownloadUrl}}'
-Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://download.skype.com/SkypeSetupFull.exe'
+﻿#Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' '{{DownloadUrl}}'
+Install-ChocolateyPackage 'skype' 'exe' '/SILENT /nogoogle /noie /nodesktopicon' 'http://www.skype.com/go/getskype-Full'
